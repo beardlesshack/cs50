84fc5b8ff07b6490e54956ea7f646ce79852d6fd
Rich Fowler <eartheus@hotmail.com>
2026-02-22 09:31:02 -0500

    Add files via upload

---
+++ address.c
@@ -0,0 +1,26 @@
+#include <cs50.h>
+#include <stdio.h>
+int main(void)
+
+
+
+{
+  //  int n = 50;
+  //  int *p = &n;
+
+ // string s = "HI!";
+  //  printf("%p\n", s);
+  //  printf("%p\n", &s[0]);
+  //  printf("%p\n", &s[1]);
+   // printf("%p\n", &s[2]);
+   // printf("%p\n", &s[3]);
+   // printf("%p\n", &s[4]);
+
+
+    char *s = "HI!";
+   // printf("%s\n", s);
+    printf("%s\n", s + 1); // dropping the [square breckets] from s[0]
+  //  printf("%c\n", *(s + 2));
+
+
+}
